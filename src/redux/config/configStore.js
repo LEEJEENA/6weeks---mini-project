1793c500cfbbce24023f1ade10cdda5d19882ad6
rlawodnr1109 <wodnr1109@naver.com>
2022-10-21 19:45:47 +0900

    첫 세팅

---
+++ src/redux/config/configStore.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import melon from "../modules/melonSlice";
+
+const store = configureStore({
+  reducer: { melon },
+
+  //배포모드에서 리덕스 데브툴 사용 안함
+  devtools: process.env.REACT_APP_MOD !== "production",
+});
+
+export default store;
---
+++ src/shared/Router.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import MyPage from "../pages/MyPage";
+import Detail from "../pages/Detail";
+import Revise from "../pages/Revise";
+import SignUp from "../pages/SignUp";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Revise/:id" element={<Revise />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/MyPage/:id" element={<MyPage />} />
+        <Route path="/Detail/:id" element={<Detail />} />
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
